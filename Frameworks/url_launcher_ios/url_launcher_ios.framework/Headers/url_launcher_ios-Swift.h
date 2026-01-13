1fbb0ca5fadd541effe7c08973a51b4373b4e1ec
Kawthar Al-Tamimi <kawthar.mujahed@hyperpay.com>
2026-01-13 13:56:33 +0300

    Release v1.0.6: Rebuild xcframework with simulator support

--- Frameworks/url_launcher_ios/url_launcher_ios.framework/Headers/url_launcher_ios-Swift.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:475fb9cf9b8470744f21d9f7614a179736412afe985a56bf7b8e1648e6ad63ff
-size 10693
--- Frameworks/webview_flutter_wkwebview/webview_flutter_wkwebview.framework/Headers/webview_flutter_wkwebview-Swift.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b3279586192217114fcfb6d2ba66dab9e010c9144d7689154e6532216398c844
-size 13743
